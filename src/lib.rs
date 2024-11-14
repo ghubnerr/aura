99f4a0542f075f655ab3c8e799675316af08db4a
gabriel hubner lucchesi <glucc002@onyx.cs.fiu.edu>
2024-11-14 17:31:43 -0500

    Fix cargo pyo3 binding by removing tokio tests

--- src/lib.rs
+++ src/lib.rs
@@ -178,3 +178,32 @@ fn aura(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<SignalingServer>()?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test] // Note: changed from tokio::test to regular test
+    fn test_server_creation() {
+        let server = SignalingServer::new(Some(3031), Some("127.0.0.1".to_string()));
+        assert_eq!(server.port, 3031);
+        assert_eq!(server.ip, "127.0.0.1");
+    }
+
+    #[test] // Note: changed from tokio::test to regular test
+    fn test_server_default_values() {
+        let server = SignalingServer::new(None, None);
+        assert_eq!(server.port, 3030);
+        assert_eq!(server.ip, "127.0.0.1");
+    }
+
+    #[test]
+    fn test_message_serialization() {
+        let offer = SignalingMessage::Offer {
+            sdp: "test_sdp".to_string(),
+        };
+        let serialized = serde_json::to_string(&offer).unwrap();
+        assert!(serialized.contains("offer"));
+        assert!(serialized.contains("test_sdp"));
+    }
+}
