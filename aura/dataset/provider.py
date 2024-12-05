d27834712d836fa8da7253e3ccbeb32efeb40171
David Ulloa <dulloa6310@gmail.com>
2024-12-04 23:54:52 -0500

    Finish provider helper for processing dataset

--- aura/dataset/provider.py
+++ aura/dataset/provider.py
@@ -1,92 +1,160 @@
 import os
 import random
 from typing import Union, List
+from collections import defaultdict, Counter
 
 import cv2
 import kagglehub
 import numpy as np
 
+CLASS_CUTOFF = 2000
+
 class DatasetProvider:
-    def __init__(self, save_dir: Union[None, str]):
-        datasets = []
-        datasets.append(kagglehub.dataset_download("jafarhussain786/human-emotionshappy-faces"), path=save_dir)
-        datasets.append(kagglehub.dataset_download("jafarhussain786/human-emotionssad-faces"), path=save_dir)
-        datasets.append(kagglehub.dataset_download("jafarhussain786/human-emotionsangry-faces"), path=save_dir)
-        datasets.append(kagglehub.dataset_download("jafarhussain786/human-emotionsfear-faces"), path=save_dir)
-        datasets.append(kagglehub.dataset_download("jafarhussain786/human-emotionssuprise-faces"), path=save_dir)
+    def __init__(self, save_dir: Union[None, str], target_size: Union[None, tuple[int, int]] = None):
+        self.target_size = target_size
+        self.emotion_labels = {
+            "happy": 0,
+            "sad": 1,
+            "anger": 2,
+            "disgust": 3,
+            "fear": 4,
+            "neutral": 5,
+            "surprise": 6,
+            "contempt": 7,
+        }
+        original_dir = os.getcwd()
 
-        self.image_paths = []
-        for path in datasets:
-            self.image_paths.extend(DatasetProvider._collect_files(path))
-
-    def get_image(self, **kwargs) -> np.ndarray:
-        """
-        Returns a random, unprocessed image from our dataset.
-        """
-        path = random.choice(self.image_paths)
-        image = None
         try:
-            image = cv2.imread(path)
+            os.chdir(save_dir)
         except:
-            print(f"Could not read file from path: {path}")
-            return None
+            print(f"Could not access to {save_dir}, saving to process directory.")
 
-        return DatasetProvider._resize_image(image)
+        dataset_path = kagglehub.dataset_download("noamsegal/affectnet-training-data")
+        os.chdir(original_dir)
 
-    def get_next_image_batch(self, **kwargs) -> List[np.ndarray]:
+        dataset = self._collect_files(dataset_path)
+
+        train_size = int(len(dataset) * .8)
+
+        self.train = dataset[:train_size]
+        self.test = dataset[train_size:]
+
+    def sample(self, index: int, test: bool) -> tuple[np.ndarray, int]:
         """
-        Returns groups of images (positives) to train SimCLR embeddings.
+        Returns an image and it's respective label given an index
         """
-        image = DatasetProvider.get_image()
-        image = DatasetProvider._set_black_background(image)
+        dataset = self.test if test else self.train
+        if index >= len(dataset):
+            raise ValueError(f"The `index` provided of value {index} is larger than the length of `self.image_paths`, {len(self.image_paths)}")
 
-        batch = []
-        batch.append(DatasetProvider._flip(image))
-        batch.append(DatasetProvider._random_brightness(image))
-        batch.append(DatasetProvider._random_zoom(image))
-        batch.append(DatasetProvider._random_rotation(image))
+        return dataset[index]
 
-        return batch
+    def get_next_image_batch(self, batch_size: int, test: bool):
+        """
+        Generator that yields batches of images from the dataset.
+        """
+        dataset = self.test if test else self.train
+        if batch_size > len(dataset):
+            raise ValueError("Can't get batch size larger than dataset")
 
-    def _collect_files(path: str) -> List[str]:
-        image_path = []
-        for f in os.listdir(path):
-            if f.endswith(('.png', '.jpg', '.jpeg')):
-                image_path.append(os.path.join(path, f))
-        return image_path 
+        indices = list(range(len(dataset)))
+        for i in range(0, len(dataset), batch_size):
+            batch_indices = indices[i:i + batch_size]
+            batch = []
+            for idx in batch_indices:
+                batch.append(self.sample(idx, test))
+            yield batch
 
-    def _resize_image(image: np.ndarray, target_size=(224, 224)) -> np.ndarray:
-        # If the image is grayscale, convert it to 3 channels (RGB)
+    def label_distr(self, test = False):
+        dataset = self.test if test else self.train
+        counter = Counter([emotion for _, _, emotion in dataset])
+        for emotion, count in counter.items():
+            print(f"{emotion}: {(count/len(dataset))*100:.2f}%")
+    
+    def _get_emotion(self, path):
+        dirs = path.split("/")
+        if len(dirs) < 2:
+            raise ValueError(f"Path, {path}, is not in proper format to parse emotion")
+        return dirs[-2]
+
+    def _collect_files(self, path: str) -> List[str]:
+        dataset = []
+        label_counter = defaultdict(int)
+        for dirpath, _, filenames in os.walk(path):
+            for filename in filenames:
+                img_path = os.path.join(dirpath, filename)
+                if not img_path.endswith((".jpg", ".png", ".jpeg")):
+                    continue
+
+                emotion = self._get_emotion(img_path)
+                if label_counter[emotion] >= 2000:
+                    continue
+                
+                label = self.emotion_labels[emotion]
+                img = cv2.imread(img_path)
+                rot_img = self._random_rotation(img)
+                flip_img = self._flip(img)
+                bright_img = self._random_brightness(img)
+                # zoom_img = self._random_zoom(img)
+
+                mods = [img, rot_img, flip_img, bright_img]
+                for mod in mods:
+                    dataset.append((mod, label, emotion))
+
+                label_counter[self._get_emotion(img_path)] += len(mods)
+
+        random.shuffle(dataset)
+        return dataset
+
+    def _resize_image(self, image: np.ndarray) -> np.ndarray:
+        if self.target_size is None:
+            print("Called `_resize_image` without set target_size.")
+            return
+
+        if not isinstance(image, np.ndarray):
+            raise ValueError("Input must be a numpy array.")
+        
+        if len(image.shape) not in [2, 3]:
+            raise ValueError("Input image must be either grayscale (2D) or RGB/BGR (3D).")
+        
         if len(image.shape) == 2:
             image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-
+        
         h, w = image.shape[:2]
-        scale = min(target_size[0] / h, target_size[1] / w)
-        new_w = int(w * scale)
-        new_h = int(h * scale)
-
-        resized_image = cv2.resize(image, (new_w, new_h))
-        canvas = np.zeros((target_size[0], target_size[1], 3), dtype=np.uint8)
-        top = (target_size[0] - new_h) // 2
-        left = (target_size[1] - new_w) // 2
+        if h == 0 or w == 0:
+            print("Image shape", image.shape)
+            print("h, w", h, w)
+            raise ValueError("Input image has invalid dimensions (height or width is zero).")
+        
+        scale = min(self.target_size[0] / h, self.target_size[1] / w)
+        new_w = max(1, int(round(w * scale)))
+        new_h = max(1, int(round(h * scale)))
+        
+        resized_image = cv2.resize(image, (new_w, new_h), interpolation=cv2.INTER_AREA)
+        
+        canvas = np.zeros((self.target_size[0], self.target_size[1], 3), dtype=np.uint8)
+        top = (self.target_size[0] - new_h) // 2
+        left = (self.target_size[1] - new_w) // 2
         canvas[top:top + new_h, left:left + new_w] = resized_image
-        return canvas
+        
+        output_image = np.ascontiguousarray(canvas, dtype=np.uint8)
+        return output_image
     
-    def _random_rotation(image: np.ndarray) -> np.ndarray:
+    def _random_rotation(self, image: np.ndarray) -> np.ndarray:
         angle = np.random.uniform(-30, 30)
         h, w = image.shape[:2]
         center = (w // 2, h // 2)
         M = cv2.getRotationMatrix2D(center, angle, 1.0)
         return cv2.warpAffine(image, M, (w, h))
     
-    def _flip(image: np.ndarray) -> np.ndarray:
+    def _flip(self, image: np.ndarray) -> np.ndarray:
         return cv2.flip(image, 1)  
     
-    def _random_brightness(image: np.ndarray) -> np.ndarray:
+    def _random_brightness(self, image: np.ndarray) -> np.ndarray:
         factor = np.random.uniform(0.5, 1.5)
         return cv2.convertScaleAbs(image, alpha=factor, beta=0)
     
-    def _random_zoom(image: np.ndarray) -> np.ndarray:
+    def _random_zoom(self, image: np.ndarray) -> np.ndarray:
         h, w = image.shape[:2]
         zoom = np.random.uniform(0.3, 2.0)  
         new_h, new_w = int(h * zoom), int(w * zoom)
@@ -97,9 +165,10 @@ class DatasetProvider:
             cropped = image[top:top + new_h, left:left + new_w]
         else:
             cropped = image[:h, :w]
-        return DatasetProvider._resize_image(cropped)
+        return self._resize_image(cropped)
     
-    def _set_black_background(image: np.ndarray, threshold=20) -> np.ndarray:
+    def _set_black_background(self, image: np.ndarray, threshold=20) -> np.ndarray:
+        image = np.ascontiguousarray(image, dtype=np.uint8)  # Ensure image is contiguous and of type uint8
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         _, mask = cv2.threshold(gray_image, threshold, 255, cv2.THRESH_BINARY_INV)
         image[mask == 255] = [0, 0, 0]
