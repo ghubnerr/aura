e1750fd6ec6fb36779670362fb15f20bb8a511a7
Gabriel <gabrielhubnerlucchesi@gmail.com>
2024-12-23 22:34:12 -0500

    Moved pytest.ini location and add pip install -e . to pytest

--- aura/__init__.py
+++ aura/__init__.py
@@ -1,14 +1,14 @@
-import os
-from dotenv import load_dotenv
+    import os
+    from dotenv import load_dotenv
 
-from aura.aura import SignalingServer, VideoStreamer
+    from aura.aura import SignalingServer, VideoStreamer
 
-load_dotenv()
-if not os.environ.get("STORAGE_PATH"):
-    os.makedirs("output", exist_ok=True)
-    os.environ["STORAGE_PATH"] = "./output"
+    load_dotenv()
+    if not os.environ.get("STORAGE_PATH"):
+        os.makedirs("output", exist_ok=True)
+        os.environ["STORAGE_PATH"] = "./output"
 
-save_dir = os.path.join(os.environ.get("STORAGE_PATH"), "aura_storage")
-os.makedirs(save_dir, exist_ok=True)
+    save_dir = os.path.join(os.environ.get("STORAGE_PATH"), "aura_storage")
+    os.makedirs(save_dir, exist_ok=True)
 
-__all__ = ["SignalingServer", "VideoStreamer"]
+    __all__ = ["SignalingServer", "VideoStreamer"]
